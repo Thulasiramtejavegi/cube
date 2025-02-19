0e3ccbba502887b473b63308dbc00baea9ce98b9
grooveinnovations-thulasiramteja <thulasiramteja.vegi@grooveinnovations.ai>
2025-02-19 19:59:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # Update package list
-sudo apt update -y
-
-# Install Docker if not installed
-if ! command -v docker &> /dev/null; then
-    sudo apt install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-fi
 
 # Run Cube.js container
 docker run -d --name cubejs-container -p 4000:4000 -p 15432:15432 \
